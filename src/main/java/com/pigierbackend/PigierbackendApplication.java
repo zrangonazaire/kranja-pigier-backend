ee5b477a0b582f617bbafaca4bd615359c3fa991
bigbossoh <ange0145@gmail.com>
2025-04-03 16:54:22 +0000

    modification du fichier medical

--- src/main/java/com/pigierbackend/PigierbackendApplication.java
+++ src/main/java/com/pigierbackend/PigierbackendApplication.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -19,63 +21,64 @@ import lombok.RequiredArgsConstructor;
 @EnableAsync
 @RequiredArgsConstructor
 @EnableScheduling
-// @OpenAPIDefinition(info = @Info(title = "Pigierbackend", version = "1.0",
-// description = "Application de Gestion de PIGIER"))
-// @SecurityScheme(name = "krinjaAuth", type = SecuritySchemeType.HTTP, in =
-// SecuritySchemeIn.HEADER, scheme = "bearer", bearerFormat = "JWT")
-public class PigierbackendApplication {
+public class PigierbackendApplication extends SpringBootServletInitializer { // Étendre SpringBootServletInitializer
 
-	public static void main(String[] args) {
-		SpringApplication.run(PigierbackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PigierbackendApplication.class); // Indiquer la classe principale
+    }
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOriginPatterns(
-			Arrays.asList(
-				"http://localhost:4200",
-				"*"
-			)
-		);
-		corsConfiguration.setAllowedHeaders(
-			Arrays.asList(
-				"Origin",
-				"Access-Control-Allow-Origin",
-				"Content-Type",
-				"Accept",	
-				"Authorization",
-				"Origin, Accept",
-				"X-Requested-With",
-				"Access-Control-Request-Method",
-				"Access-Control-Request-Headers",
-				"Jwt-Token",
-				"Content-Disposition"
-			)
-		);
-		corsConfiguration.setExposedHeaders(
-			Arrays.asList(
-				"Origin",
-				"Content-Type",
-				"Accept",	
-				"Authorization",
-				"Access-Control-Allow-Origin",
-				"Access-Control-Allow-Credentials",
-				"Content-Disposition"
-			)
-		);
-		corsConfiguration.setAllowedMethods(
-			Arrays.asList(
-				"GET",
-				"POST",
-				"PUT",
-				"DELETE",
-				"OPTIONS"
-			)
-		);
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PigierbackendApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOriginPatterns(
+            Arrays.asList(
+                "http://localhost:4200",
+                "*"
+            )
+        );
+        corsConfiguration.setAllowedHeaders(
+            Arrays.asList(
+                "Origin",
+                "Access-Control-Allow-Origin",
+                "Content-Type",
+                "Accept",	
+                "Authorization",
+                "Origin, Accept",
+                "X-Requested-With",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers",
+                "Jwt-Token",
+                "Content-Disposition"
+            )
+        );
+        corsConfiguration.setExposedHeaders(
+            Arrays.asList(
+                "Origin",
+                "Content-Type",
+                "Accept",	
+                "Authorization",
+                "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials",
+                "Content-Disposition"
+            )
+        );
+        corsConfiguration.setAllowedMethods(
+            Arrays.asList(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+            )
+        );
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
