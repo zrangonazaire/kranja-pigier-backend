ea6bd3202ac943866eb475251e4773f138b19544
zrangonazaire <61186442+zrangonazaire@users.noreply.github.com>
2025-09-02 13:01:57 +0000

    correction état

--- src/main/java/com/pigierbackend/anneeacademique/AnneeScolaireController.java
+++ src/main/java/com/pigierbackend/anneeacademique/AnneeScolaireController.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/annees-academiques")
@@ -20,14 +19,14 @@ public class AnneeScolaireController {
     final AnneeScolaireService anneeScolaireService;
     // Define endpoints for saving and retrieving academic years here
     // Example:
-    @PostMapping ("/save-annee-scolaire")
+    @PostMapping (value="/save-annee-scolaire",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AnneeScolaireDto> saveAnneeScolaire(@RequestBody AnneeScolaireDto anneeScolaireDto) {
         AnneeScolaireDto savedAnnee = anneeScolaireService
             .saveAnneeScolaire(anneeScolaireDto);
         return ResponseEntity.ok(savedAnnee);
     }
 
-    @GetMapping("/liste-annees-scolaires")
+    @GetMapping(value="/liste-annees-scolaires",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AnneeScolaireDto>> getListeAnneesScolaires() {
         List<AnneeScolaireDto> annees = anneeScolaireService.getAllAnneesScolaires();
         return ResponseEntity.ok(annees);
